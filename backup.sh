3b268cb80b0efaa8e7bd1d3eea645606c901b3e6
Owen <owenmcmonagle@protonmail.com>
2025-02-15 10:44:22 +0000

    #3:         Fixed bug where extra parameters were truncated after initial shorthand -aAXHvx.         Limited IO bandwidth to 20MB p/s.         Wrapped rsync in ionice with setting to only read/write when disk isn't busy.

--- backup.sh
+++ backup.sh
@@ -3,19 +3,21 @@
 # Enable stop on error for script and pipeline chain.
 set -eo
 
-source=$1
-target=$2
-extra_params=$3
+source="$1"
+target="$2"
+shift 2
 
 backup_directory() {
 
-	local source=$1
-	local target=$2
-	local extra_params=$3
+	local source="$1"
+	local target="$2"
+	shift 2
+	local params=("$@")
 
 	echo "Syncing input $source to destination $target"
-	rsync $extra_params $source/ $target/
+	echo "Params: ${params[*]}"
+	ionice -c3 rsync "${params[@]}" "$source/" "$target/"
 	echo "Syncing complete"
 }
 
-backup_directory $source $target $extra_params
+backup_directory "$source" "$target" "$@"
